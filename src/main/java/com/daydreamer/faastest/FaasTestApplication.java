001ce656f6be9fb5bd695b38412422d6e5625852
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2024-12-05 11:14:40 +0800

    INIT

---
+++ src/main/java/com/daydreamer/faastest/FaasTestApplication.java
@@ -0,0 +1,13 @@
+package com.daydreamer.faastest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaasTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaasTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daydreamer/faastest/FaasTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daydreamer.faastest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaasTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
