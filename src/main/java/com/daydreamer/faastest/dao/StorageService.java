e18b3f84e205c8dc21c1d77a03eab3b70fc88573
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2024-12-15 15:23:24 +0800

    update: 新增dao层接口

---
+++ src/main/java/com/daydreamer/faastest/dao/StorageService.java
@@ -0,0 +1,26 @@
+package com.daydreamer.faastest.dao;
+
+import com.daydreamer.faastest.entity.ServiceFunction;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public interface StorageService {
+    /**
+     * 储存API函数的名字，函数体，参数名及参数默认值
+     * @param serviceFunction
+     * @return
+     */
+    boolean storageServiceFunction(ServiceFunction serviceFunction);
+    boolean deleteServiceFunction(UUID serviceFunctionId);
+    ServiceFunction getServiceFunction(UUID serviceFunctionId);
+
+    /**
+     * 分页查询部署的部分云函数
+     * @param start
+     * @param pageSize
+     * @return
+     */
+    ArrayList<ServiceFunction> getServiceFunctionByPaging(Integer start, Integer pageSize);
+    Integer getServiceFunctionCount();
+}
