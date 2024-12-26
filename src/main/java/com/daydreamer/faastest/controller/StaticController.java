3ce9518a13a2733ffe711bf20bf1b5457f9ba04b
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2024-12-26 12:09:20 +0800

    save

---
+++ src/main/java/com/daydreamer/faastest/controller/StaticController.java
@@ -0,0 +1,12 @@
+package com.daydreamer.faastest.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@Slf4j
+@RestController("/APIStreamStaticResources")
+public class StaticController {
+
+}
