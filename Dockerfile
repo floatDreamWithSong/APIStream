74ca10a627b16030c75204f6ea497fde7d09aebb
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-01-03 15:13:53 +0800

    published

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# 使用OpenJDK 21作为基础镜像
+FROM eclipse-temurin:21-jdk
+
+## 设置环境变量
+#ENV SPRING_OUTPUT_ANSI_ENABLED=ALWAYS \
+#    JDBC_DATABASE_URL=jdbc:mysql://mysql:3306/your_database?useSSL=false \
+#    JDBC_DATABASE_USER=your_username \
+#    JDBC_DATABASE_PASSWORD=your_password \
+#    MINIO_ENDPOINT=minio:9000 \
+#    MINIO_ACCESS_KEY=your_access_key \
+#    MINIO_SECRET_KEY=your_secret_key
+
+# 将Spring Boot应用添加到镜像中
+ADD target/APIStream-0.0.1-SNAPSHOT.jar app.jar
+
+# 暴露8080端口（Spring Boot默认端口）
+EXPOSE 8080
+
+# 启动Spring Boot应用
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/com/daydreamer/apistream/ApiStreamApplicationTests.java
+++ src/test/java/com/daydreamer/apistream/ApiStreamApplicationTests.java
@@ -17,23 +17,6 @@ class ApiStreamApplicationTests {
     @Test
     void tryJsTask() throws IOException {
         System.out.print(JsonProcessor.gson.toJson(ModulePath.resolvePath("/project/modu/test/test::testfn")));
-
-    }
-    @Autowired
-    private UserMapper userMapper;
-
-    @Test
-    public void testSelect() {
-        System.out.println(("----- selectAll method test ------"));
-        List<User> userList = userMapper.selectList(null);
-        userList.forEach(System.out::println);
-        userList = userMapper.selectList(null);
-        userList.forEach(System.out::println);
-    }
-
-    @Test
-    public void testConvert() {
-
     }
 
 }
