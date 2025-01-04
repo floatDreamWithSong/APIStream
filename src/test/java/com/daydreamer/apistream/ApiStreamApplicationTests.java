a879cd41f800f6fcf1db2782918a78a76b85f5e4
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-01-04 17:04:15 +0800

    ignore unreleated file

--- src/test/java/com/daydreamer/apistream/ApiStreamApplicationTests.java
+++ src/test/java/com/daydreamer/apistream/ApiStreamApplicationTests.java
@@ -1,22 +1,13 @@
 package com.daydreamer.apistream;
 
-import com.daydreamer.apistream.common.JsonProcessor;
-import com.daydreamer.apistream.common.ModulePath;
-import com.daydreamer.apistream.entity.User;
-import com.daydreamer.apistream.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.io.IOException;
-import java.util.List;
 
 
 @SpringBootTest
 class ApiStreamApplicationTests {
 
-    @Test
-    void tryJsTask() throws IOException {
-        System.out.print(JsonProcessor.gson.toJson(ModulePath.resolvePath("/project/modu/test/test::testfn")));
-    }
+
 
 }
